728b08c15a9b2e30924cc43da0b530a4f7c784bc
condyl <Condyl678@gmail.com>
2024-02-29 22:02:26 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 const mineflayer = require('mineflayer')
 const mineflayerViewer = require('prismarine-viewer').mineflayer
+const inventoryViewer = require('mineflayer-web-inventory')
 
 const bot = mineflayer.createBot({
   host: 'localhost', // minecraft server ip
   username: 'Bot', // username to join as if auth is `offline`, else a unique identifier for this account. Switch if you want to change accounts
-  auth: 'microsoft' // for offline mode servers, you can set this to 'offline'
-  // port: 25565,              // set if you need a port that isn't 25565
+  auth: 'microsoft', // for offline mode servers, you can set this to 'offline'
+  port: 25565,              // set if you need a port that isn't 25565
   // version: false,           // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
   // password: '12345678'      // set if you want to use password-based auth (may be unreliable). If specified, the `username` must be an email
 })
 
-bot.on('chat', (username, message) => {
-    if (username === bot.username) return
-    bot.chat(message)
-  })
 
 bot.once('spawn', () => {
-    mineflayerViewer(bot, { port: 3007, firstPerson: true })
+    mineflayerViewer(bot, { port: 3007, firstPerson: true });
+    inventoryViewer(bot)
   })
