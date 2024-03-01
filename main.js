001b1f39105ac382f83061649400016e9c0da9d5
condyl <Condyl678@gmail.com>
2024-03-01 01:00:32 -0500

    change

--- main.js
+++ main.js
@@ -1,18 +1,98 @@
-const mineflayer = require('mineflayer')
-const mineflayerViewer = require('prismarine-viewer').mineflayer
-const inventoryViewer = require('mineflayer-web-inventory')
+const mineflayer = require("mineflayer");
+const mineflayerViewer = require("prismarine-viewer").mineflayer;
+const {
+    pathfinder,
+    Movements,
+    goals: { GoalNear },
+} = require("mineflayer-pathfinder");
 
 const bot = mineflayer.createBot({
-  host: 'localhost', // minecraft server ip
-  username: 'Bot', // username to join as if auth is `offline`, else a unique identifier for this account. Switch if you want to change accounts
-  auth: 'microsoft', // for offline mode servers, you can set this to 'offline'
-  port: 25565,              // set if you need a port that isn't 25565
-  // version: false,           // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
-  // password: '12345678'      // set if you want to use password-based auth (may be unreliable). If specified, the `username` must be an email
-})
+    host: process.argv[2],
+    port: parseInt(process.argv[3]),
+    auth: "microsoft",
+});
 
+bot.loadPlugin(pathfinder);
 
-bot.once('spawn', () => {
+bot.once("spawn", () => {
     mineflayerViewer(bot, { port: 3007, firstPerson: true });
-    inventoryViewer(bot)
-  })
+});
+
+bot.once("spawn", () => {
+    const defaultMove = new Movements(bot);
+    const RANGE_GOAL = 0;
+
+    bot.on("chat", (username, message) => {
+        if (username === bot.username) return;
+        if (message !== "come") return;
+        const target = bot.players[username]?.entity;
+        if (!target) {
+            bot.chat("I don't see you !");
+            return;
+        }
+        const { x: playerX, y: playerY, z: playerZ } = target.position;
+
+        bot.pathfinder.setMovements(defaultMove);
+        bot.pathfinder.setGoal(
+            new GoalNear(playerX, playerY, playerZ, RANGE_GOAL)
+        );
+    });
+
+    bot.on("chat", (username, message) => {
+        if (username === bot.username) return;
+		const coords = message.split(' ');
+		if (coords[0] !== 'goto') return;
+		for (let i = 0; i < coords.length; i++) {
+			coords[i] = parseInt(coords[i])
+		}
+		bot.pathfinder.setMovements(defaultMove)
+		bot.pathfinder.setGoal(new GoalNear(coords[1], coords[2], coords[3], RANGE_GOAL))
+    });
+});
+
+bot.on("spawn", () => {
+    bot.on("chat", (username, message) => {
+        if (message === "attack me") attackPlayer(username);
+        else if (message === "attack") attackEntity();
+        else if (message === "jump") jump();
+    });
+});
+
+function jump() {
+    if (bot.getControlState("jump") === true)
+        bot.setControlState("jump", false);
+    else if (bot.getControlState("jump") === false)
+        bot.setControlState("jump", true);
+}
+
+function attackPlayer(username) {
+    if (username === bot.username) return;
+    const player = bot.players[username];
+    if (!player || !player.entity) {
+        bot.chat("I can't see you");
+    } else {
+        bot.chat(`Attacking ${player.username}`);
+        bot.attack(player.entity);
+    }
+}
+
+function attackEntity() {
+    const entity = bot.nearestEntity();
+    if (!entity) {
+        bot.chat("No nearby entities");
+    } else if (
+        entity.type === "player" ||
+        entity.type === "passive" ||
+        entity.type === "hostile" ||
+        entity.type === "animal"
+    ) {
+        bot.chat(`Attacking ${entity.name ?? entity.username}`);
+        bot.attack(entity);
+    } else {
+        bot.chat(`No nearby mobs/players`);
+    }
+}
+
+// Log errors and kick reasons:
+bot.on("kicked", console.log);
+bot.on("error", console.log);
